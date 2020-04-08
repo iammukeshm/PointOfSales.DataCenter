1d53455c18a50c53f5b559e704597f219703be01
Mukesh Murugan <iammukeshm@gmail.com>
2020-04-08 21:05:51 +0530

    added product,group and category

---
+++ PointOfSales.DataDistribution.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using PointOfSales.DataDistribution.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSales.DataDistribution.Domain.Entities
+{
+    public class Product :AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+        public bool DisableIfStockIsZero { get; set; } = true;
+        public ProductGroup ProductGroup { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+    }
+}
