fddae2cca3b8445a6000b06ed2072135ac34883c
Mukesh Murugan <iammukeshm@gmail.com>
2020-04-08 23:35:31 +0530

    added  repository pattern, generic repo implementation and sample for product repo async

---
+++ PointOfSales.DataDistribution.Application/Interfaces/IRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSales.DataDistribution.Application.Interfaces
+{
+    public interface IRepositoryAsync<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        //Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        //Task<int> CountAsync(ISpecification<T> spec);
+    }
+}
---
+++ PointOfSales.DataDistribution.Infrastructure.Persistence/Repositories/ProductRepositoryAsync.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PointOfSales.DataDistribution.Domain.Entities;
+using PointOfSales.DataDistribution.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSales.DataDistribution.Infrastructure.Persistence.Repositories
+{
+    //public class ProductRepositoryAsync : GenericRepositoryAsync<Product>, IProductRepositoryAsync
+    //{
+    //    public ProductRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+    //    {
+    //    }
+
+    //    public Task<Product> GetByIdWithItemsAsync(int id)
+    //    {
+    //        return _dbContext.Products
+    //            .Include(o => o.)
+    //            .Include($"{nameof(Order.OrderItems)}.{nameof(OrderItem.ItemOrdered)}")
+    //            .FirstOrDefaultAsync(x => x.Id == id);
+    //    }
+    //}
+}
