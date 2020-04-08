b702c8cbac660f1fca535e9fdc1dff9feb1734f1
Mukesh Murugan <iammukeshm@gmail.com>
2020-04-08 20:41:40 +0530

    implemented api versioning

---
+++ PointOfSales.DataDistribution/Controllers/CoreController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSales.DataDistribution.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class CoreController : ControllerBase
+    {
+        
+    }
+}
--- PointOfSales.DataDistribution/Startup.cs
+++ PointOfSales.DataDistribution/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -71,6 +72,19 @@ namespace PointOfSales.DataDistribution
           }
         });
 
+            });
+            #endregion
+
+            #region Api Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
             });
             #endregion
 
