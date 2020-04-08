839e04e2f551f213fb8b0d535b07add8809c3cb6
Mukesh Murugan <iammukeshm@gmail.com>
2020-04-08 19:53:04 +0530

    Add project files.

---
+++ PointOfSales.DataDistribution.Infrastructure.Identity/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSales.DataDistribution.Infrastructure.Identity
+{
+    class DependencyInjection
+    {
+    }
+}
